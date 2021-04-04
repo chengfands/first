ff255d29c9daa657750f61928318992b68c1023e
chengfan <1476153957@qq.com>
2021-04-04 19:38:27 +0800

    study

--- src/main/java/com/cf/base/Math.java
+++ src/main/java/com/cf/base/Math.java
@@ -6,6 +6,9 @@ package com.cf.base;
  */
 public class Math {
 
+    /**
+     * 运算
+     */
     public static void main(String[] args) {
         System.out.println(1 & 2);
         System.out.println(1 & 3);
@@ -15,5 +18,35 @@ public class Math {
         System.out.println(1 ^ 3);
         System.out.println(~ 3);
         System.out.println(-2 >>> 2);
+    }
+}
+
+class Math2{
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+        System.out.println(reverse(-123000));
+        System.out.println(reverse(123000));
+        System.out.println(reverse(12003000));
+        System.out.println(reverse(-12003000));
+    }
+
+    /**
+     * 数字反转
+     * @author chengfan
+     */
+    private static int reverse(int a){
+        boolean f = true;
+        if(a < 0){
+            f = false;
+            a = -a;
+        }
+        if(a == 0){
+            return a;
+        }
+        StringBuffer buffer = new StringBuffer(String.valueOf(a));
+        buffer.reverse();
+        return f?Integer.parseInt(buffer.toString()):-Integer.parseInt(buffer.toString());
     }
 }
